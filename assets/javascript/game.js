47dd22309424552142594be97a1965d68820cf4d
cynthialu42 <clu2334@gmail.com>
2018-02-28 14:06:53 -0600

    added images and changing question

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,12 @@
 //List of hangman words
 var availWords = ["cockatiel", "parrot", "duck","penguin"];
+var jokeQ = ["cockatiel","parrot","duck","penguin"];
 
 // chosen string
-var selectedWord = availWords[Math.floor(Math.random() * availWords.length)];
+var chosenIndex = Math.floor(Math.random() * availWords.length);
+var selectedWord = availWords[chosenIndex];
+var selectedQuestion = jokeQ[chosenIndex];
+
 var wordArr = [];
 var guessed = [];
 var countDown = selectedWord.length *2;
@@ -12,6 +16,7 @@ var displayWord = document.getElementById("display-word");
 var lettersGuessed = document.getElementById("letters-guessed");
 var guessesLeft = document.getElementById("guesses-left");
 var userText = document.getElementById("user-text");
+var jokeQuestion = document.getElementById("joke-question");
 
 //get an array of underscores
 for (let i = 0; i < selectedWord.length; i++){ 
@@ -20,9 +25,24 @@ for (let i = 0; i < selectedWord.length; i++){
 //join gets rid of the , in the array
 displayWord.innerHTML = wordArr.join(" ");
 
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+function changeImage() {
+const key = document.querySelector(".left-image");
+
+key.classList.add('playing');
+}
+
+//const keys = Array.from(document.querySelectorAll('.key'));
+//keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//window.addEventListener('keydown', playSound);
 
 //Get user input
 document.onkeyup = function(event) {
+    jokeQuestion.innerHTML = selectedQuestion;
     userText.textContent = event.key;
     //look for selected letter, if found, put it in wordArr
     for (let i = 0; i < selectedWord.length; i++) {
@@ -48,10 +68,14 @@ document.onkeyup = function(event) {
     else if(countDown>0){
         guessesLeft.innerHTML = countDown;
     }
-    else if (countDown === 0)
-        guessesLeft.innerHTML = "Game Over! \n Press any key to play again";
-    else
+    else if (countDown === 0){
+        guessesLeft.innerHTML = "Game Over! Press any key to play again";
+        changeImage();
+    }  
+    else{
         location.reload();
+    }
+        
 }
 
 
