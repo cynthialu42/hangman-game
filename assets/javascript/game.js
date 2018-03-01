b1b91f04462a6b3eaaf231b065ba07d5a13c2d8c
cynthialu42 <clu2334@gmail.com>
2018-03-01 14:41:43 -0600

    generally finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,13 @@
 //List of hangman words
-var jokeQ = ["What do you give a sick bird?","What kind of math do birds do?","What do you call an uncomfortable situation?","How many birds can play at this game?"];
-var availWords = ["tweetment", "owlgebra", "hawkward","toucan"];
+//var jokeQ = ["What do you give a sick bird?","What kind of math do birds do?","What do you call an uncomfortable situation?","How many birds can play at this game?"];
+//var availWords = ["tweetment", "owlgebra", "hawkward","toucan"];
 
-
-// chosen string
-var chosenIndex = Math.floor(Math.random() * availWords.length);
-var selectedWord = availWords[chosenIndex];
-var selectedQuestion = jokeQ[chosenIndex];
-
-var wordArr = [];
-var guessed = [];
-var countDown = selectedWord.length *2;
-var win = 0;
+var wordArr;;
+var guessed;;
+var countDown;
+var selectedWord;
+var selectedQuestion;
+var wins = 0;
 
 //set up variables for printing to screen
 var displayWord = document.getElementById("display-word");
@@ -21,54 +17,131 @@ var userText = document.getElementById("user-text");
 var jokeQuestion = document.getElementById("joke-question");
 var totalWins = document.getElementById("total-wins");
 
-//get an array of underscores
-for (let i = 0; i < selectedWord.length; i++){ 
-    wordArr.push("_ ");
+function reset(){
+    wordArr = [];
+    guessed = [];
+    lettersGuessed.innerHTML = guessed;
 }
-//join gets rid of the , in the array
-displayWord.innerHTML = wordArr.join(" ");
 
+function initGame(){
+    reset();
+    var jokeQ = [
+        "What do you give a sick bird?",
+        "What kind of math do birds do?",
+        "What do you call an uncomfortable situation?",
+        "How many birds can play at this game?",
+        "What do birds use instead of zippers?",
+        "Largest bird in the world",
+        "Hoot hoot!",
+        "Coo coo~",
+        "Waddle waddle sliiiide!",
+        "What kind of bird is this guy over here?"
+
+    ];
+    var availWords = [
+        "tweetment", 
+        "owlgebra", 
+        "hawkward",
+        "toucan",
+        "velcrow",
+        "ostrich",
+        "owl",
+        "pigeon",
+        "penguin",
+        "kakapo"
+
+    ];
+    var chosenIndex = Math.floor(Math.random() * availWords.length);
+
+    selectedWord = availWords[chosenIndex];
+    selectedQuestion = jokeQ[chosenIndex];
+    countDown = selectedWord.length *2;
+
+    for (let i = 0; i < selectedWord.length; i++){ 
+        wordArr.push("_ ");
+    }
+    jokeQuestion.innerHTML = selectedQuestion;
+    displayWord.innerHTML = wordArr.join(" ");
+    guessesLeft.innerHTML = countDown;
+
+    changeImage(countDown);
+}
 
 function changeImage(countDown) {
-    const key = document.querySelector(".start-image");
-    key.classList.add('hide');
-    if(countDown === -1){
-        const win = document.querySelector(".win-image");
-        win.classList.add('show');
-    }
-    else{
-        const fail = document.querySelector(".fail-image");
-        fail.classList.add('show');
-    }
-    
-}
-//Get user input
 
-document.onkeyup = function(event) {   
-    jokeQuestion.innerHTML = selectedQuestion;
-    userText.textContent = event.key;
-    //look for selected letter, if found, put it in wordArr
-    for (let i = 0; i < selectedWord.length; i++) {
-        if (event.key == selectedWord.charAt(i)){
-            wordArr.splice(i,1,event.key);   
+    const start = document.querySelector(".start-image");
+    const win = document.querySelector(".win-image");
+    const fail = document.querySelector(".fail-image");
+    const bonus = document.querySelector(".bonus-image");
+    const dark = document.querySelector(".background-img");
+    const tweed = document.querySelector(".background-img");
+    var pinkWords = document.querySelectorAll(".words,.title");
+    const pinkBorder = document.querySelector(".right-column");
+
+    if(countDown === -1){
+        if (wins%5 === 0){
+            start.classList.add('hide');
+            bonus.classList.add('show');
+            dark.classList.add('background-dark');
+            for(let i = 0; i < pinkWords.length; i++){
+                pinkWords[i].style.color = "rgb(135, 255, 133)";
+            }
+            pinkBorder.classList.add('pink-border');
+        }
+        else{
+            start.classList.add('hide');
+            win.classList.add('show');
+            dark.classList.add('background-dark');
+            for(let i = 0; i < pinkWords.length; i++){
+                pinkWords[i].style.color = "rgb(255, 0, 221)";
+            }
+            pinkBorder.classList.add('pink-border');
+        }
+        
+    }
+    else if (countDown === 0){
+        start.classList.add('hide');
+        fail.classList.add('show');
+        tweed.classList.add('background-grey');
+        for(let i = 0; i < pinkWords.length; i++){
+            pinkWords[i].style.color = "black";
         }
     }
-    //add the letter to the guessed array
-    guessed.push(event.key);
+    else{
+        start.classList.remove('hide');
+        win.classList.remove('show');
+        fail.classList.remove('show');
+        bonus.classList.remove('show');
+        dark.classList.remove('background-dark');
+        tweed.classList.remove('background-grey');
+        for(let i = 0; i < pinkWords.length; i++){
+            pinkWords[i].style.color = "black";
+        }
+        pinkBorder.classList.remove('pink-border');
+    }
+}
 
-    //decrement the number of guesses left
-    countDown--;
+//Get user input
+function runGame(selectedKey){
 
-    //show the stuff on screen
-    displayWord.innerHTML = wordArr.join(" ");
+    for (let i = 0; i < selectedWord.length; i++) {
+        if (selectedKey == selectedWord.charAt(i)){
+            wordArr.splice(i, 1, selectedKey);
+        }
+    }
+
+    guessed.push(selectedKey);
     lettersGuessed.innerHTML = guessed.join(" ");
+
+    displayWord.innerHTML = wordArr.join(" ");
     
-    //determine if win or lose
+    countDown--;
+    
     if(countDown>0 && (wordArr.join("")==selectedWord)){
         guessesLeft.innerHTML = "You Win! Press any key to play again";
         countDown = -1;
-        win++;
-        totalWins.innerHTML = win;
+        wins++;
+        totalWins.innerHTML = wins;
         changeImage(countDown);
     }
     else if(countDown>0){
@@ -80,10 +153,26 @@ document.onkeyup = function(event) {
         changeImage(countDown);
     }  
     else{
-        location.reload();
+        initGame();
     }
-        
 }
+
+document.onkeyup = function(event) {   
+    var keyPress = event.key;  
+    runGame(keyPress);    
+}
+
+function reload(){
+    location.reload();
+}
+
+
+initGame();
+
+
+
+
+
 
 
 
